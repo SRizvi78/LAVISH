58eca0379924ae751a77017c20a1c2c496f9987f
srizvi78 <srnowroz78@gmail.com>
2020-12-09 18:04:26 +0600

    nothin

--- src/components/pages/Services/Services.js
+++ src/components/pages/Services/Services.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeroSection from '../../HeroSection';
 import Pricing from '../../Pricing';
-import { homeObjFour, homeObjOne, homeObjThree, homeObjTwo } from './Data';
+import { homeObjOne} from './Data';
 
 function Services() {
     return (
