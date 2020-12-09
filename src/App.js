1a0da1af9e9f41cb74ebe38c6369cd24fabaacb4
srizvi78 <srnowroz78@gmail.com>
2020-12-09 17:25:34 +0600

    deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/pages/HomePage/Home';
+import Footer from './components/pages/Footer/Footer';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/Sign-Up/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router className="App">
+      <Navbar />
+      <Switch>
+      <Route path='/' exact component={Home}/>  
+      <Route path='/products' component={Products} />
+      {<Route path='/sign-up' component={SignUp} / >}
+      <Route path='/services' exact component={Services}/>    
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
